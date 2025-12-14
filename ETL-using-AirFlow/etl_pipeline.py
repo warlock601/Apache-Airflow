2aaa01c19b68a95d7a8a986cc0b7ebfd9157a17d
Vivek Raj <vr32288@gmail.com>
2025-12-14 07:19:20 +0530

    Added functions for extract_apod, transform_apod_data, load_data_to_postgres & defined dependencies in etl_pipeline.py
    Files changed:
    - etlpipeline.py

--- ETL-using-AirFlow/etl_pipeline.py
+++ ETL-using-AirFlow/etl_pipeline.py
@@ -2,13 +2,13 @@ from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 import json
 
 ## Define the DAG
 with DAG(
     dag_id='nasa_apod_postgres',
-    start_date=days_ago(1),
+    start_date=datetime(2025,12,14),
     schedule='@daily',
     catchup=False
 ) as dag:
@@ -47,15 +47,51 @@ with DAG(
 
 
     ## Step 3: Transform the data (pick the info that I need to save)
-
+    @task
+    def transform_apod_data(response):
+        apod_data={
+            'title': response.get('title',''),
+            'explanation': response.get('explanation',''),
+            'url': response.get('url',''),
+            'date': response.get('date',''),
+            'media_type': response.get('media_type','')
+        }
+        return apod_data
 
 
     ## Step 4: Load the data into Postgres SQL
+    @task
+    def load_data_to_postgres(apod_data):
 
+        ## Initialize the PostgresHook
+        postgres_hook=PostgresHook(postgres_conn_id='my_postgres_connection')
 
+        ## Define the SQL Insert query
+        insert_query="""
+        INSERT INTO apod_data (title, explanation, url, date, media_type)
+        VALUES (%s, %s, %s, %s, %s);
+        """
+
+        ## Execute the SQL query
+        postgres_hook.run(insert_query,parameters=(
+            apod_data['title'],
+            apod_data['title'],
+            apod_data['title'],
+            apod_data['title'],
+            apod_data['title']
+        ))
 
     ## Step 5: Verify the data DBViewer
 
 
 
     ## Step 6: Define the task dependencies.
+    create_table_task=create_table()
+    #create_table >> extract_apod      # Ensure the table is created before extraction
+    api_response=extract_apod.output
+    # transform
+    transformed_data=transform_apod_data(api_response)
+    # load
+    load_data_task=load_data_to_postgres(transformed_data)
+
+    create_table_task >> extract_apod >> transformed_data >> load_data_task
